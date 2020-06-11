1fc25c7043506be93ffa5cc7e62c7070286673aa
artem <artem.dubovov@bk.ru>
2020-06-11 09:55:10 +0300

    First

--- js/script.js
+++ js/script.js
@@ -9,13 +9,6 @@ $(document).ready(function(){
       });
   });
 
-$(window).load(function() {
-  var images = $('img');
-    images.each(function(i) {
-      $(this).width($(this).width() / 2);
-    });
-});
-
 
 let main2 = document.querySelector('.main__menu');
 let item = main2.querySelectorAll('.main__menu-item');
