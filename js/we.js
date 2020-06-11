d9f51f6533e8723378d29b240477f847f5be455a
artem <artem.dubovov@bk.ru>
2020-06-11 14:39:11 +0300

    First

---
+++ js/we.js
@@ -0,0 +1,9 @@
+let main = document.querySelector('.main__menu');
+let item = main.querySelectorAll('.main__menu-item');
+
+item.forEach(el => {
+  el.onclick = function (){
+    main.querySelector('.main__menu-item-active').classList.remove('main__menu-item-active');
+    el.classList.add('main__menu-item-active');
+  }
+});
