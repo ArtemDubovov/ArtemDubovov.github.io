edc77c801300d9f1b9e34e0642401b0e7bafec4f
artem <artem.dubovov@bk.ru>
2020-06-14 18:53:09 +0300

    test1

--- js/we.js
+++ js/we.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $('ul.main__menu').on('click', 'li:not(.main__menu-item-active)', function() {
     $(this)
       .addClass('main__menu-item-active').siblings().removeClass('main__menu-item-active')
-      .closest('div.container').find('div.we__content').removeClass('we_content-active').eq($(this).index()).addClass('we_content-active');
+      .closest('div.container-we').find('div.we__content').removeClass('we_content-active').eq($(this).index()).addClass('we_content-active');
   });
   
 });
