6e113fff4e8b50968bffd7614a7e5fe6f87346af
artem <artem.dubovov@bk.ru>
2020-06-14 11:34:15 +0300

    test1

--- js/we.js
+++ js/we.js
@@ -1,16 +1,26 @@
 //Изменение стилей в меню main
+$(document).ready(function() {
+  
+  $('ul.main__menu').on('click', 'li:not(.main__menu-item-active)', function() {
+    $(this)
+      .addClass('main__menu-item-active').siblings().removeClass('main__menu-item-active')
+      .closest('div.container').find('div.we__content').removeClass('we_content-active').eq($(this).index()).addClass('we_content-active');
+  });
+  
+});
+
 
 let main = document.querySelector('.main__menu');
 let item = main.querySelectorAll('.main__menu-item');
+let wrap = document.querySelector('.we__wrapper');
+let wrapItem = document.querySelector('.we__content');
 
 
+// item.forEach(el => {
+//   el.onclick = function (){
+//     main.querySelector('.main__menu-item-active').classList.remove('main__menu-item-active');
+//     el.classList.add('main__menu-item-active');
+//   }
+// });
 
-item.forEach(el => {
-  el.onclick = function (){
-    main.querySelector('.main__menu-item-active').classList.remove('main__menu-item-active');
-    el.classList.add('main__menu-item-active');
-    wrapper.querySelector('.we_content-active').classList.remove('we_content-active');
-    weItem.el.classList.add('we_content-active');
-  }
-});
 
